bb20301ef614a49c062bc1aed51ec98a912af511
Julien Margarido <julien.margarido@ext.saint-gobain.com>
2019-01-16 16:50:32 +0100

    renamed projects, added ng-bullet dep in With-* projects

---
+++ With-NgBullet-JHipsterMonolithic/src/main/java/fr/bullet/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package fr.bullet.repository;
+
+import fr.bullet.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
